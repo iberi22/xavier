c6f57873139199a788fa8e148dbf22d1fc6a248b
beri <beri22@gmail.com>
2026-08-08 03:23:14 -0500

    chore: quitar scripts de patch del agente (ruido del PR #1235, no código)

--- tests_path_traversal.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cat << 'INNER_EOF' > tests/v1_memories_add_path_traversal.rs
-use xavier::server::v1_api::{v1_memories_add, V1AddMemoryRequest, V1AddParams};
-// Placeholder, the other test is already passing and I will just document it.
-INNER_EOF
